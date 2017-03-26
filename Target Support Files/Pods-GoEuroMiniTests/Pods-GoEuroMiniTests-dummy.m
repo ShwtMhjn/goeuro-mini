0bcfa1a93f60f142ab6686c54f2bcf93d159a6a6
Shweta Mahajan <shweta_mahajan1987@yahoo.com>
2017-03-27 03:31:53 +0530

    Initial Commit

---
+++ Target Support Files/Pods-GoEuroMiniTests/Pods-GoEuroMiniTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoEuroMiniTests : NSObject
+@end
+@implementation PodsDummy_Pods_GoEuroMiniTests
+@end
---
+++ Target Support Files/Pods-GoEuroMiniUITests/Pods-GoEuroMiniUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoEuroMiniUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GoEuroMiniUITests
+@end
